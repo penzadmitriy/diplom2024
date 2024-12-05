519351bf2359740b5c48be27ea38459d6b3e859b
Дмитрий <penzadmitry@gmail.com>
2024-12-05 16:39:07 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://fc51e491-f4cd-4e45-8af6-f9762e2ff174.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+
+CREAT_NEW_ORDERS = "/api/v1/orders"
---
+++ test_automation.py
@@ -0,0 +1,24 @@
+# Дмитрий Малышев.24qa +. Финальный проект.
+import configuration
+import requests
+import data
+# Создание заказа
+def create_new_order(body):
+    return requests.post (configuration.URL_SERVICE + configuration.CREAT_NEW_ORDERS,
+                         json=body)
+# Получение заказа по номеру трекера
+def get_order(track_number):
+    get_order_url = f"{configuration.URL_SERVICE}/api/v1/orders/track?t={track_number}"
+    response = requests.get(get_order_url)
+    return response
+# Автотест
+def test_order_creation_and_retrieval():
+    response = create_new_order(data.order_body)
+    track_number = response.json()["track"]
+    print("Заказ создан. Номер заказа:", track_number)
+# Получение данных заказа по треку
+    order_response = get_order(track_number)
+    assert order_response.status_code == 200
+    order_data = order_response.json()
+    print("Данные заказа:")
+    print(order_data)
